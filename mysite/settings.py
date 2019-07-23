840fe5e02af783b9fed42be9332bb87e13545c0a
msdeep14 <mandeepsinghshekhawat95@gmail.com>
2019-07-23 18:46:55 +0530

    remove static

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,9 +157,9 @@ SIMPLE_JWT = {
 }
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
